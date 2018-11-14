5d47f4a7ff420c0680b86bee701000884d2b8d6b
Conner Chessmore <connerchessmore@gmail.com>
2018-11-14 16:30:54 -0600

    Tweaked vote.php

--- vote.php
+++ vote.php
@@ -3,20 +3,15 @@ require_once('lib/mysqlclass.php');
 
 $db = new mySQL();
 $db->connect('localhost','sacad','root');
-if(isset($_POST['poster']))
+if(isset($_POST['poster'])&&isset($_POST['userID']))
 {
 	$poster = $db->select("SELECT id FROM posters WHERE code = ?",[$_POST['poster']]);
-	$user = $db->select("SELECT id FROM users WHERE email = ?",[$_POST['user']]);
 	if($poster->rowCount() == 0) {
-		echo("Error. No table found with that code.");
-	}
-	if($user->rowCount() == 0) {
-		echo("Error. No user found with that email.");
+		echo("Error. No poster found with that code.");
 	}
 	else {
 		
 		$posterID = $poster->fetchColumn();
-		$userID = $user->fetchColumn();
-		$db->insert("INSERT INTO votes(user_id, poster_id) VALUES (?, ?)",[$userID,$posterID]);
+		$db->insert("INSERT INTO votes(user_id, poster_id) VALUES (?, ?)",[$_POST['userID'],$posterID]);
 	}
 }
