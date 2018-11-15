69681682e13975f3405a5bbfed5a792c280c7d08
Nicholas Caporusso <ncaporusso@gmail.com>
2018-11-14 23:09:31 -0600

    Fixed qr code generation (client)

--- server/checkin.php
+++ server/checkin.php
@@ -1,12 +1,12 @@
 <?php
 header('Content-Type: application/json');
-if(isset($_POST['fname']{0}) && isset($_POST['lname']{0}) && isset($_POST['uemail']{0})){
+if(isset($_POST['f']{0}) && isset($_POST['l']{0}) && isset($_POST['e']{0})){
 	require_once('settings.php');
 	require_once('lib/mysqlclass.php');
 	$sql=new mySQL();
 	$db->connect($settings['db/host'],$settings['db/name'],$settings['db/user'],$settings['db/password']);
-	$result=$db->select('SELECT ID FROM users WHERE email=?',$_POST['uemail']);
+	$result=$db->select('SELECT ID FROM users WHERE email=?',$_POST['e']);
 	if($result->rowCount()>0) die(json_encode(['status'=>'OK','user_id'=>$sth->fetchColumn()]));
-	$result=$db->insert('INSERT INTO users(email,firstname,lastname) VALUES(?,?,?)',[$_POST['uemail'],$_POST['fname'],$_POST['lname']]);
+	$result=$db->insert('INSERT INTO users(email,firstname,lastname) VALUES(?,?,?)',[$_POST['e'],$_POST['f'],$_POST['l']]);
 	die(json_encode(['status'=>'OK','user_id'=>$row['ID']]));
 }
